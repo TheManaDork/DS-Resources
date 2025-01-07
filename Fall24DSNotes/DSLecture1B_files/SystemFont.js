0e81a232270810b035323228628c46ba7758afa2
TheManaDork <129630859+TheManaDork@users.noreply.github.com>
2025-01-07 16:16:14 -0500

    Add files via upload

---
+++ Fall24DSNotes/DSLecture1B_files/SystemFont.js
@@ -0,0 +1,39 @@
+﻿(function () {
+    document.addEventListener('DOMContentLoaded', function () {
+
+        //get root
+        var systemFont = 'Default'
+        var r = document.querySelector(':root');
+        
+        if (window?.App?.SystemFont) {
+            systemFont = window.App.SystemFont;
+            if (systemFont === 'OpenDyslexic' || systemFont === 'Dyslexie') {
+                r.style.setProperty('--site-font-family', 'OpenDyslexic Regular')
+            } else {
+                r.style.setProperty('--site-font-family', 'inherit')
+            }
+        }
+        else if (window?.ApplicationInitialization?.SystemFont) {
+            systemFont = window.ApplicationInitialization.SystemFont;
+            if (systemFont === 'OpenDyslexic' || systemFont === 'Dyslexie') {
+                r.style.setProperty('--site-font-family', 'OpenDyslexic Regular')
+            } else {
+                r.style.setProperty('--site-font-family', 'inherit')
+            }
+        }
+        else if (window?.SiteData?.SystemFont) {
+            systemFont = window.SiteData.SystemFont;
+            if (systemFont === 'OpenDyslexic' || systemFont === 'Dyslexie') {
+                r.style.setProperty('--site-font-family', 'OpenDyslexic Regular')
+                r.style.setProperty('--custom-font-family-gilroy', 'OpenDyslexic Regular')
+                r.style.setProperty('--custom-font-family-verdana', 'OpenDyslexic Regular')
+                r.style.setProperty('--custom-font-family-proximanova', 'OpenDyslexic Regular')
+            } else {
+                r.style.setProperty('--site-font-family', 'inherit')
+                r.style.setProperty('--custom-font-family-gilroy', 'Gilroy, Verdana, sans-serif')
+                r.style.setProperty('--custom-font-family-verdana', 'Verdana, Helvetica, sans-serif')
+                r.style.setProperty('--custom-font-family-proximanova', 'proxima-nova, Verdana, sans-serif')
+            }
+        }
+    });
+})();
